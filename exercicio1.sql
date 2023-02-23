e07846c351b9cf8195f4bd5f8764d9cd270b800d
andreas <drehanter13@gmail.com>
2023-02-23 20:52:54 -0300

    first commit

---
+++ exercicio1.sql
@@ -0,0 +1,7 @@
+use fundos;
+
+select * from aplicaçao a
+join cliente cl on a.CodCli = cl.CodCli
+join fundinvest fi on a.NumFundo = fi.NumFundo
+join consultor co on a.NumConsultor = co.NumConsultor
+join tipofundo tf on fi.CodTipoFundo = tf.CodTipoFundo;
